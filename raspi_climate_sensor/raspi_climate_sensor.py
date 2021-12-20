babb1d7437fb4727614f5da20429b87f958ab6de
Anton Sherstiuk <anton.sher@gmail.com>
2021-12-20 15:44:40 +0100

    fixing font

--- raspi_climate_sensor/raspi_climate_sensor.py
+++ raspi_climate_sensor/raspi_climate_sensor.py
@@ -12,15 +12,14 @@ PIN_RED = 26
 PIN_YELLOW = 6
 PIN_GREEN = 5
 
-font = None
-
 def initialize():
     leds.setup_pin_for_led(PIN_GREEN)
     leds.setup_pin_for_led(PIN_YELLOW)
     leds.setup_pin_for_led(PIN_RED)
     font = ImageFont.truetype('/usr/share/fonts/truetype/liberation2/LiberationSans-Regular.ttf', 18)
+    return font
 
-def make_image(temperature, pressure, humidity):
+def make_image(font, temperature, pressure, humidity):
     img = Image.new('RGB', (display.image_width, display.image_height))
     draw = ImageDraw.Draw(img)
     draw.text((10, 5),  f"{temperature:6.1f} °C", font=font, fill=(255, 255, 255))
@@ -28,13 +27,13 @@ def make_image(temperature, pressure, humidity):
     draw.text((10, 85), f"{pressure:6.1f} hPa", font=font, fill=(255, 255, 255))
     return img
 
-def main_cycle():
+def main_cycle(font):
     temperature, pressure, humidity = hygrometer.read_temp_pressure_humidity()
-    image = make_image(temperature, pressure, humidity)
+    image = make_image(font, temperature, pressure, humidity)
     display.display_image(image)
 
 if __name__ == "__main__":
-    initialize()
+    font = initialize()
     while True:
-        main_cycle()
+        main_cycle(font)
         time.sleep(30)
