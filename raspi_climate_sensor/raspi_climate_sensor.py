0766fd155eb8671c463b3b7d43e764527b8ac9f5
Anton Sherstiuk <anton.sher@gmail.com>
2021-12-30 22:59:08 +0100

    change display to no leds, no pressure, background for status

--- raspi_climate_sensor/raspi_climate_sensor.py
+++ raspi_climate_sensor/raspi_climate_sensor.py
@@ -6,46 +6,34 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-import leds,hygrometer,display,co2meter
-
-PIN_RED = 26
-PIN_YELLOW = 6
-PIN_GREEN = 5
+import hygrometer,display,co2meter
 
 def initialize():
-    leds.setup_pin_for_led(PIN_GREEN)
-    leds.setup_pin_for_led(PIN_YELLOW)
-    leds.setup_pin_for_led(PIN_RED)
-    font = ImageFont.truetype('/usr/share/fonts/truetype/liberation2/LiberationSans-Bold.ttf', 20)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/liberation2/LiberationSans-Bold.ttf', 30)
     return font
 
-def make_image(font, temperature, pressure, humidity, co2level):
-    img = Image.new('RGB', (display.image_width, display.image_height))
+def make_image(font, status_color, temperature, pressure, humidity, co2level):
+    img = Image.new(mode='RGB', size=(display.image_width, display.image_height), color=status_color)
     draw = ImageDraw.Draw(img)
-    draw.text((10, 5),  f"{temperature:.1f} °C", font=font, fill=(255, 255, 255))
-    draw.text((10, 35), f"{humidity:.0f} %", font=font, fill=(255, 255, 255))
-    draw.text((10, 65), f"{pressure:.0f} hPa", font=font, fill=(255, 255, 255))
-    draw.text((10, 95), f"{co2level:.0f} ppm", font=font, fill=(255, 255, 255))
-    draw.text((10, 125), f"{time.strftime('%H:%M:%S')}", font=font, fill=(255, 255, 255))
+    draw.ellipse((80, 15, 110, 45), fill=status_color, outline = status_color)
+    draw.text((5, 0),  f"{temperature:.0f}°", font=font, fill=(0, 0, 0))
+    draw.text((5, 40), f"{humidity:.0f}%", font=font, fill=(0, 0, 0))
+    draw.text((5, 80), f"{co2level:.0f} ppm", font=font, fill=(0, 0, 0))
+    draw.text((5, 120), f"{time.strftime('%H:%M:%S')}", font=font, fill=(0, 0, 0))
     return img
 
 def main_cycle(font):
     temperature, pressure, humidity = hygrometer.read_temp_pressure_humidity()
     co2level = co2meter.get_co2_level()
+    status_color = 'grey'
     if co2level <= 800:
-        leds.set_led_status_on_pin(PIN_RED, 0)
-        leds.set_led_status_on_pin(PIN_YELLOW, 0)
-        leds.set_led_status_on_pin(PIN_GREEN, 1)
-    elif humidity <= 1200:
-        leds.set_led_status_on_pin(PIN_RED, 0)
-        leds.set_led_status_on_pin(PIN_YELLOW, 1)
-        leds.set_led_status_on_pin(PIN_GREEN, 0)
+        status_color = 'green'
+    elif co2level <= 1200:
+        status_color = 'yellow'
     else:
-        leds.set_led_status_on_pin(PIN_RED, 1)
-        leds.set_led_status_on_pin(PIN_YELLOW, 0)
-        leds.set_led_status_on_pin(PIN_GREEN, 0)
+        status_color = 'red'
     for _ in range(30):
-        image = make_image(font, temperature, pressure, humidity, co2level)
+        image = make_image(font, status_color, temperature, pressure, humidity, co2level)
         display.display_image(image)
         time.sleep(1)
 
