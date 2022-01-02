efa0d70a28d89feecfe861bd757949fe1e90cf24
Anton Sherstiuk <anton.sher@gmail.com>
2022-01-02 14:55:53 +0100

    catch exceptions when reading bme280

--- raspi_climate_sensor/raspi_climate_sensor.py
+++ raspi_climate_sensor/raspi_climate_sensor.py
@@ -16,14 +16,22 @@ def make_image(font, status_color, temperature, pressure, humidity, co2level):
     img = Image.new(mode='RGB', size=(display.image_width, display.image_height), color=status_color)
     draw = ImageDraw.Draw(img)
     draw.ellipse((80, 15, 110, 45), fill=status_color, outline = status_color)
-    draw.text((5, 0),  f"{temperature:.0f}°", font=font, fill=(0, 0, 0))
-    draw.text((5, 40), f"{humidity:.0f}%", font=font, fill=(0, 0, 0))
+    if temperature is not None:
+        draw.text((5, 0),  f"{temperature:.0f}°", font=font, fill=(0, 0, 0))
+        draw.text((5, 40), f"{humidity:.0f}%", font=font, fill=(0, 0, 0))
+    else:
+        draw.text((5, 0),  f"???°", font=font, fill=(0, 0, 0))
+        draw.text((5, 40), f"???%", font=font, fill=(0, 0, 0))
     draw.text((5, 80), f"{co2level:.0f} ppm", font=font, fill=(0, 0, 0))
     draw.text((5, 120), f"{time.strftime('%H:%M:%S')}", font=font, fill=(0, 0, 0))
     return img
 
 def main_cycle(font):
-    temperature, pressure, humidity = hygrometer.read_temp_pressure_humidity()
+    temperature, pressure, humidity = None, None, None
+    try:
+        temperature, pressure, humidity = hygrometer.read_temp_pressure_humidity()
+    except Exception as e:
+        print("Could not read BME280 sensor:", repr(e), "\n")
     co2level = co2meter.get_co2_level()
     status_color = 'grey'
     if co2level <= 800:
